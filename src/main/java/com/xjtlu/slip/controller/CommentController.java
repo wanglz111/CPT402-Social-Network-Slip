3c056f71950d3047b15d6349c0ad05cf2d3e3c4d
lucas <1339696209@qq.com>
2022-05-14 11:28:56 +0800

    add reply comment part

--- src/main/java/com/xjtlu/slip/controller/CommentController.java
+++ src/main/java/com/xjtlu/slip/controller/CommentController.java
@@ -1,16 +1,18 @@
 package com.xjtlu.slip.controller;
 
 import com.xjtlu.slip.pojo.Comment;
+import com.xjtlu.slip.pojo.User;
 import com.xjtlu.slip.service.CommentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
-@RestController
+@Controller
+@Slf4j
 public class CommentController {
     @Autowired
     private CommentService commentService;
@@ -18,5 +20,14 @@ public class CommentController {
     @GetMapping("/comment/{topicId}")
     public List<Comment> getComment(@PathVariable() String topicId){
         return commentService.getListByTopicId(topicId);
+    }
+
+    @PostMapping("/addComment")
+    public String addComment(@RequestParam("topicId") String topicId, @RequestParam("replyContent") String content, @RequestParam("userId") String replyUserId, HttpSession session){
+        User loginUser = (User) session.getAttribute("loginUser");
+        // 判断用户与评论的用户是否一致，防止post攻击
+        assert loginUser.getId().toString().equals(replyUserId);
+        log.info("topicId:"+topicId+" content:"+content+" replyUserId:"+replyUserId);
+        return "redirect:/topic/d/"+topicId;
     }
 }
