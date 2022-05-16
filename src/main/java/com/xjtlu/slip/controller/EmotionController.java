86060722548ce93b581133ccc27c4f6a6c000daf
lucas <1339696209@qq.com>
2022-05-16 20:02:54 +0800

    add emotion details page

--- src/main/java/com/xjtlu/slip/controller/EmotionController.java
+++ src/main/java/com/xjtlu/slip/controller/EmotionController.java
@@ -1,16 +1,24 @@
 package com.xjtlu.slip.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.xjtlu.slip.pojo.Emotion;
 import com.xjtlu.slip.pojo.User;
 import com.xjtlu.slip.service.EmotionService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Enumeration;
 
 @Controller
+@Slf4j
 public class EmotionController {
 
     @Autowired
@@ -31,8 +39,31 @@ public class EmotionController {
     }
 
     @PostMapping("/delEmotion")
-    public String addEmotion(@RequestParam("id") Long id, HttpSession session) {
+    public String addEmotion(@RequestParam("id") Long id, HttpSession session, HttpServletRequest request) {
         emotionService.removeById(id);
-        return "redirect:/";
+        String referer = request.getHeader("Referer");
+        String redirectURI = referer.replace(request.getServerName(), "");
+        return "redirect:" + redirectURI;
+    }
+
+    @GetMapping("/emotion/{current}")
+    public String emotion(HttpSession session, Model model,@PathVariable("current") Integer current) {
+        User user = (User) session.getAttribute("loginUser");
+        QueryWrapper<Emotion> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("user_id", user.getId());
+        queryWrapper.orderByDesc("create_time");
+        Page<Emotion> page = new Page<>(current, 10);
+        page = emotionService.page(page, queryWrapper);
+        page.getRecords().forEach(emotion -> {
+                    LocalDateTime time = LocalDateTime.ofEpochSecond(emotion.getCreateTime(), 0, ZoneOffset.of("+8"));
+                    emotion.setTime(String.valueOf(time));
+                });
+        model.addAttribute("page", page);
+        return "emotion";
+    }
+
+    @GetMapping("/emotion")
+    public String emotion() {
+        return "redirect:/emotion/1";
     }
 }
